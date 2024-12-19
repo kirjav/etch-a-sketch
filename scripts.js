75930c40cc4d6b4a4b93b0482341e7d868c62855
kirjav <89679609+kirjav@users.noreply.github.com>
2024-12-19 15:36:17 -0600

    Deprecated background hover effect and replaced with drawing event.

--- scripts.js
+++ scripts.js
@@ -3,26 +3,32 @@ let currentGridSize = window.innerWidth > 768 ? 70 : 30;
 var mouseDown = false;
 let eraser = false;
 let sketchbook = document.querySelector(".sketchbook");
+let lastElement = null;
 
 document.querySelector('#opacityRange').addEventListener('input', (e) => {
     opacitySetting = e.target.value / 100;
 })
-//MOUSE EVENTS
-sketchbook.onmousedown = () => (mouseDown = true)
-sketchbook.onmouseup = () => (mouseDown = false)
-// MOBILE EVENTS
+//PC MOUSE EVENTS
+sketchbook.onmousedown = (event) => {
+    mouseDown = true;
+    draw(event);
+}
+sketchbook.addEventListener('mousemove', draw);
+document.body.onmouseup = () => (mouseDown = false)
+document.addEventListener('dragstart', (event) => {
+    event.preventDefault();
+  });
 
+// MOBILE EVENTS
 sketchbook.ontouchstart = (event) => {
     event.preventDefault(); // Prevent default touch behavior (e.g., scrolling)
     mouseDown = true;
     draw(event);
 };
-sketchbook.addEventListener('touchmove', draw);
-sketchbook.ontouchend = () => (mouseDown = false)
 
-document.addEventListener('dragstart', (event) => {
-    event.preventDefault();
-  });
+sketchbook.addEventListener('touchmove', draw);
+document.body.ontouchend = () => (mouseDown = false)
+
 
 function refreshGrid() {
     let numberOfSquares = prompt("Please enter the desired number of squares per side: ");
@@ -58,48 +64,6 @@ function clearGrid() {
     generateGrid(currentGridSize);
 }
 
-function changeBackground(event) {
-    if(mouseDown){
-        if(!eraser){
-            colorToUse = document.getElementById("colorChoice").value;
-            event.target.classList.add('hovered');
-            let newOpacityValue = Number(event.target.style.opacity) + opacitySetting;
-            event.target.style.opacity = newOpacityValue;
-            event.target.style.backgroundColor = colorToUse;
-        } else {
-            event.target.classList.remove('hovered');
-            let newOpacityValue = "";
-            event.target.style.opacity = newOpacityValue;
-            event.target.style.backgroundColor = "";
-        }
-    }
-}
-
-function generateGrid(numberOfSquares) {
-    const sketchbook = document.querySelector(".sketchbook");
-    document.querySelector('#scaleReference').innerHTML = "Scale: " + numberOfSquares + " x " + numberOfSquares;
-    sketchbook.innerHTML = '';
-    let numberOfRows = numberOfSquares;
-    currentGridSize = numberOfSquares;
-
-    while (numberOfRows > 0) {
-        let numberOfColumns = numberOfSquares;
-        const divRow = document.createElement("div");
-        divRow.classList.add("sketch_box_row");
-        sketchbook.appendChild(divRow);
-
-        while (numberOfColumns > 0) {
-            const div = document.createElement("div");
-            div.classList.add("sketch_box_element");
-            div.addEventListener("mouseover", changeBackground);
-            //div.addEventListener("touchstart", changeBackground);
-            divRow.appendChild(div);
-            numberOfColumns -= 1;
-        }
-        numberOfRows -= 1;
-    }
-}
-
 function draw(event) {
     if (!mouseDown) return;
 
@@ -115,7 +79,8 @@ function draw(event) {
 
     // Determine the element at the touch/mouse location
     const element = document.elementFromPoint(clientX, clientY);
-    if (element && element.classList.contains('sketch_box_element')) {
+    if (element && element !== lastElement && element.classList.contains('sketch_box_element')) {
+        lastElement = element;
         if(!eraser){
             colorToUse = document.getElementById("colorChoice").value;
             element.classList.add('hovered');
@@ -130,5 +95,32 @@ function draw(event) {
         }
     }
 }
+
+function generateGrid(numberOfSquares) {
+    const sketchbook = document.querySelector(".sketchbook");
+    lastElement = null;
+    document.querySelector('#scaleReference').innerHTML = "Scale: " + numberOfSquares + " x " + numberOfSquares;
+    sketchbook.innerHTML = '';
+    let numberOfRows = numberOfSquares;
+    currentGridSize = numberOfSquares;
+
+    while (numberOfRows > 0) {
+        let numberOfColumns = numberOfSquares;
+        const divRow = document.createElement("div");
+        divRow.classList.add("sketch_box_row");
+        sketchbook.appendChild(divRow);
+
+        while (numberOfColumns > 0) {
+            const div = document.createElement("div");
+            div.classList.add("sketch_box_element");
+            //div.addEventListener("mouseover", changeBackground);
+            //div.addEventListener("touchstart", changeBackground);
+            divRow.appendChild(div);
+            numberOfColumns -= 1;
+        }
+        numberOfRows -= 1;
+    }
+}
+
 sketchbook.style.cursor = 'crosshair';
 generateGrid(currentGridSize);
